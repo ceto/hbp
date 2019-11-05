e224038684064bf522a35dcafb28ec2f0a12c585
Gabor Szabo <szabogabi@gmail.com>
2019-11-05 16:22:13 +0100

    subpages added

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,15 +1,15 @@
-import $ from 'jquery';
-import whatInput from 'what-input';
+import $ from "jquery";
+import whatInput from "what-input";
 
 window.$ = $;
 window.jQuery = $;
 
-import Foundation from 'foundation-sites';
+import Foundation from "foundation-sites";
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 //import './lib/foundation-explicit-pieces';
 
-require('owl.carousel');
+require("owl.carousel");
 
 // import libs from './lib/dependencies';
 // window.libs = libs;
@@ -23,49 +23,60 @@ require('owl.carousel');
 
 $(document).foundation();
 
-
-
-$(document).ready(function(){
-  var tabletbp = Foundation.MediaQuery.get('tablet'),
-    xlargebp = Foundation.MediaQuery.get('xlarge');
-  $('.owl-testi').owlCarousel({
-      loop:false,
-      dots: true,
-      responsive: {
-        0:{
-            items:1,
-            margin:20
-
-        },
-        640: {
-            items:2,
-            margin: 60
-        },
-        1200: {
-          items:3,
-          margin:80
-        }
+$(document).ready(function() {
+  var tabletbp = Foundation.MediaQuery.get("tablet"),
+    xlargebp = Foundation.MediaQuery.get("xlarge");
+  $(".owl-testi").owlCarousel({
+    loop: false,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1,
+        margin: 20
+      },
+      640: {
+        items: 2,
+        margin: 60
+      },
+      1200: {
+        items: 3,
+        margin: 80
       }
-    });
-
-    $('.owl-slider').owlCarousel({
-      autoWidth:true,
-      loop:true,
-      nav: true,
-      navText: ['‹', '›'],
-      //navContainer: '#slidernav',
-      dots: false,
-      responsive:{
-        0:{
-            items:1,
-
-        },
-        tabletbp: {
-            items:2,
-        },
-        xlargebp: {
-          items:3,
-        }
-      }
-    });
+    }
   });
+
+  $(".owl-tourlist").owlCarousel({
+    loop: false,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1,
+        margin: 20
+      },
+      640: {
+        items: 2,
+        margin: 60
+      }
+    }
+  });
+
+  $(".owl-slider").owlCarousel({
+    autoWidth: true,
+    loop: true,
+    nav: true,
+    navText: ["‹", "›"],
+    //navContainer: '#slidernav',
+    dots: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      tabletbp: {
+        items: 2
+      },
+      xlargebp: {
+        items: 3
+      }
+    }
+  });
+});
